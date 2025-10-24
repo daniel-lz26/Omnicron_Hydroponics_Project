a7745855b54db55283ce9a17b11fd6349f4cdcb2
Jasmine Vu <kevinvu@Kevins-Air-170.lan>
2025-10-24 00:25:05 -0700

    Started DB, created table and made first four columns measuring water, nutrients, ph, and time

--- BackEnd/app/models.py
+++ BackEnd/app/models.py
@@ -1,2 +1,22 @@
 # File uses SQLAlchemy to define database models for a web application.
 # it basic terms this file is where we make the db tables.
+
+# imports tools for SQLAlchemy (python library)
+from sqlalchemy import Column, Integer, Float, DateTime, String
+# imports a tool that creates a base template for all models
+from sqlalchemy.ext.declarative import declarative_base
+# tool that collects data in real time and saves it
+
+Base = declarative_base()
+# creates base that all models will inherit from, connects python classes to actual database tables
+
+
+class SensorReading(Base):
+    __tablename__ = "sensor_readings"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    water_level = Column(Float, nullable=False)
+    nutrient_level = Column(Float, nullable=False)
+    ph_level = Column(Float, nullable=True)
+    timestamp = Column(DateTime, default=datetime.utcnow, nullable=False)
