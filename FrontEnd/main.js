a24cdd1f7460219dc6c9c9eabffaed8375387356
bryamphan <bryamphan@csu.fullerton.edu>
2025-10-27 18:27:30 -0700

    Added button hovering functionality.

---
+++ FrontEnd/main.js
@@ -0,0 +1,7 @@
+// button functionality
+document.getElementById('home-button').onclick = function() {
+    document.getElementById('analytics-section').style.display = 'none';
+};
+document.getElementById('analytics-button').onclick = function() {
+    document.getElementById('analytics-section').style.display = 'block';
+};
