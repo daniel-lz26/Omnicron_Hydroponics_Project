04cf87888d098a06aaad19915fa0e7518e2e71b4
noah <noahabass13@gmail.com>
2025-10-29 00:50:31 -0700

    finished schemas file code

--- BackEnd/app/schemas.py
+++ BackEnd/app/schemas.py
@@ -1,1 +1,22 @@
 # Schemas.py file is for PYDANTIC models which is basically data validation for API requests and responses
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ProbeReadingBase(BaseModel):
+   water_level: float
+   nutrient_level: float
+   ph_level: float
+
+
+class ProbeReadingCreate(ProbeReadingBase):
+   pass
+
+
+class ProbeReadingResponse(ProbeReadingBase):
+   id: int
+   timestamp: datetime
+
+
+   class Config:
+       orm_mode = True
