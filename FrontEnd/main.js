e67d681afbdae57ce29758cecd30343b16b51148
bryamphan <bryamphan@csu.fullerton.edu>
2025-11-03 23:34:32 -0800

    button functionality

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,21 +1,21 @@
-// button functionality
-document.getElementById('home-button').onclick = function() {
-    document.getElementById('analytics-section').style.display = 'none';
-};
-document.getElementById('analytics-button').onclick = function() {
-    document.getElementById('analytics-section').style.display = 'block';
-};
+// page switch
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page-section');
+    pages.forEach(page => {
+        page.style.display = 'none';
+    })
 
-// subtitle text typing animation
-//const subtitle = document.getElementById('subtitle');
-//const text = "Omicron Class Fall 2025";
-let i = 0;
-subtitle.textContent = "";
-function typeWriter() {
-    if (i < text.length) {
-        subtitle.textContent += text.charAt(i);
-        i++;
-        setTimeout(typeWriter, 80);
+    document.getElementById(pageId + '-section').style.display = 'block';
+
+    if (pageId == 'analytics') {
+        setTimeout(() => {
+            // chart initialization
+        })
     }
 }
-window.addEventListener('DOMContentLoaded', typeWriter);
+
+window.onload = function() {
+    showPage('home');
+}
+
+
