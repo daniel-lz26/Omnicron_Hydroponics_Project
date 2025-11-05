a48bb71ba25bfe06c495aa828f0b64c3985ab9c8
Jasmine Vu <pjvu2005@gmail.com>
2025-11-05 08:41:41 -0800

    Started on main.py

--- BackEnd/app/main.py
+++ BackEnd/app/main.py
@@ -1,1 +1,27 @@
-# For Fast API init and startup
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from datetime import datetime
+from typing import List, Optional
+
+import crud
+import schemas
+from database import engine, get_db, Base
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Hydroponic System API"
+    description="API for monitoring hydroponic garden system",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_orgins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Creating the endpoints for the API
