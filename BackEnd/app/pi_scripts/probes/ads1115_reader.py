dd6f6401bdaa053df08af27a000bf44926cee142
Jasmine Vu <pjvu2005@gmail.com>
2025-11-11 18:42:26 -0800

    debugging pump control file

--- BackEnd/app/pi_scripts/probes/ads1115_reader.py
+++ BackEnd/app/pi_scripts/probes/ads1115_reader.py
@@ -5,10 +5,10 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c, address = 0x48)
+ads = ADS.ADS1115(i2c, address=0x48)
 ads.gain = 1
 
-#Use numeric channel indices instead of ADS.P0-ADS.P3
+# Use numeric channel indices instead of ADS.P0-ADS.P3
 A0 = AnalogIn(ads, 0)
 A1 = AnalogIn(ads, 1)
 A2 = AnalogIn(ads, 2)
@@ -19,11 +19,12 @@ print('\nReading ADS Sensors')
 try:
     while True:
         print(
-            f"unused: {A0.voltage:.3f} V | "
-            f"PH: {A1.voltage:.3f} V| "
-            f"Water Level: {A2.voltaage:.3f} | V"
-            f"IDS: {A3.voltage:.3f} V"
+            f"Unused: {A0.voltage:.3f} V | "
+            f"pH: {A1.voltage:.3f} V| "
+            f"Water Level: {A2.voltage:.3f} | V "
+            f"TDS: {A3.voltage:.3f} V"
         )
         time.sleep(0.5)
 except KeyboardInterrupt:
     print("\nStopped reading sensors.")
+    print("Exiting...")
--- BackEnd/app/pi_scripts/probes/pump_control.py
+++ BackEnd/app/pi_scripts/probes/pump_control.py
@@ -1,4 +1,4 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 RELAY_PIN = 18  # connecting to GPIO18 (pin 12)
@@ -8,7 +8,7 @@ GPIO.setup(RELAY_PIN, GPIO.OUT)
 
 def pump_on():
     GPIO.output(RELAY_PIN, GPIO.LOW)
-    print("Pump tured ON")
+    print("Pump turned ON")
 
 
 def pump_off():
@@ -16,8 +16,18 @@ def pump_off():
     print("Pump turned OFF")
 
 
-if __name__ = "__main__":
-    pump_on()
-    time.sleep(5)
-    pump_off()
-    GPIO.cleanup()
+if __name__ == "__main__":
+    try:
+        print("Testing pump control...")
+        print("Turning pump ON for 5 seconds.")
+        pump_on()
+        time.sleep(5)
+        print("Turning pump OFF.")
+        pump_off()
+
+        print("Test complete!")
+    except KeyboardInterrupt:
+        print("\nInterrupted by user")
+    finally:
+        GPIO.cleanup()
+        print("GPIO cleaned up")
