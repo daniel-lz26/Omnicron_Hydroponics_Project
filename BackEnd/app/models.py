c265f0631d98b6fdd0c8ab768cad761971ecf880
jenjentran15 <tranj1215@gmail.com>
2025-11-15 01:39:28 -0800

    Fix models.py after accidental corruption

--- BackEnd/app/models.py
+++ BackEnd/app/models.py
@@ -1,14 +1,16 @@
-water_level = Column(Float)
-from sqlalchemy import Column, Integer, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.sql import func
 from app.database import Base
 
 
 class ProbeReading(Base):
     __tablename__ = "probe_readings"
-    
-    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    id = Column(Integer, primary_key=True, index=True)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+
+    # Store status ONLY ("IN WATER", "OUT OF WATER", "MID LEVEL")
     water_level = Column(String)
-    nutrient_level = Column(Float, nullable=False)
-    ph_level = Column(Float, nullable=False)
-    timestamp = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    nutrient_level = Column(Float)
+    ph_level = Column(Float)
