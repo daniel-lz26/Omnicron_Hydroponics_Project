4a8bd0f2b824efd3910c9fda6c652e46da002ea6
bryamphan <bryamphan@csu.fullerton.edu>
2025-11-15 23:41:34 -0800

    Final commit.

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -23,7 +23,7 @@ function scrollToSection(sectionId) {
         const typingElement = document.getElementById('typing-text');
         if (typingElement) {
             setTimeout(() => {
-                typeText(typingElement, "Grow with us.", 75);
+                typeText(typingElement, "Grow with us. 🌱", 75);
             }, 500);
         }
     }
@@ -50,7 +50,7 @@ window.onload = function() {
     const typingElement = document.getElementById('typing-text');
     if (typingElement) {
         setTimeout(() => {
-            typeText(typingElement, "Grow with us.", 75);
+            typeText(typingElement, "Grow with us. 🌱", 75);
         }, 1000)
     }
 }
